c3199399638ec1e1c1bc83113d02773f49c5ceed
vitor_muniz <vitor_muniz@n1075.hst.br>
2020-04-29 14:59:49 -0300

    adding tables

--- back-end/src/main/java/br/com/hst/desafio3/domain/Company.java
+++ back-end/src/main/java/br/com/hst/desafio3/domain/Company.java
@@ -1,18 +1,77 @@
 package br.com.hst.desafio3.domain;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Company {
+public class Company implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY, generator="seq_company")
 	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String code;
+	@Column(nullable = false)
+	private String phone;
+	@Column(nullable = false)
+	private String type;
 	
 	public Company() {
 		
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
--- back-end/src/main/java/br/com/hst/desafio3/domain/User.java
+++ back-end/src/main/java/br/com/hst/desafio3/domain/User.java
@@ -1,18 +1,81 @@
 package br.com.hst.desafio3.domain;
 
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity
-public class User {
+public class User implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY, generator="seq_user")
 	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@OneToOne(cascade=CascadeType.REMOVE)
+	@JoinColumn(name = "company_ID", nullable = false)
+	private Company company;
 	
 	public User() {
 		
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
