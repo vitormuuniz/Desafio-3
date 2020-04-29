35b37cb5c0613e8cd34d1a68482c85dc47e3e59f
Vitor Natariani Muniz <vitor_muniz@hst.br>
2020-04-29 14:12:41 -0300

    adding directories

---
+++ back-end/src/test/java/br/com/hst/desafio3/Desafio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hst.desafio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
