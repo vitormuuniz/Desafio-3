dff7faa4497bcfdd6e0c5c419adf6677083f310f
Vinicius Dornelles de Sousa <vinicius_sousa@hst.br>
2020-05-04 16:14:09 -0300

    Custom button and imput global.css

--- front-end/src/components/Footer/index.js
+++ front-end/src/components/Footer/index.js
@@ -7,9 +7,11 @@ import logoImg from '../../assets/logoHST.png';
 
 export default function Footer(){
     return(
-        <footer> 
-            <img src={logoImg} alt="HST LOGO"/>
-            <p>Copyright &copy; 2019 - HST Card technology - All Rights Reserved</p>
-        </footer>
+        <div className="footer">
+            <footer> 
+                <img src={logoImg} alt="HST LOGO"/>
+                <p>Copyright &copy; 2019 - HST Card technology - All Rights Reserved</p>
+            </footer>
+        </div>
     );
 }
--- front-end/src/pages/NewUser/index.js
+++ front-end/src/pages/NewUser/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './styles.css';
-import '../../global.css';
+
 
 import hstLogo from '../../assets/logoHST.png';
 
@@ -9,32 +9,32 @@ export default function NewUser(){
     return(
         <div className="newUser-container">
             <div className="content">
-                <section>
-                    <img src={hstLogo} alt="HST Card Technology" />
-                </section>
+                <img src={hstLogo} alt="HST Card Technology" />
                 
-                <form action="">
-                    <div className="basicInfo">
-                        <label htmlFor="userName">Usuário:</label><br/>
-                        <input type="text" id="userName"/>
-                        <label htmlFor="companyName">Empresa: </label>
-                        <select name="companyName" id="companyName">
-                            <option value="empresa1">Empresa 1</option>
-                            <option value="empresa2">Empresa 2</option>
-                            <option value="empresa3">Empresa 3</option>
-                            <option value="empresa4">Empresa 4</option>
-                        </select>
-                    </div>
+                <section className="form">
+                        <form action="">
+                            <div className="basicInfo">
+                                <label htmlFor="userName">Usuário:</label><br/>
+                                <input type="text" id="userName"/><br/>
+                                <label htmlFor="companyName">Empresa: </label><br/>
+                                <select name="companyName" id="companyName">
+                                    <option value="empresa1">Empresa 1</option>
+                                    <option value="empresa2">Empresa 2</option>
+                                    <option value="empresa3">Empresa 3</option>
+                                    <option value="empresa4">Empresa 4</option>
+                                </select>
+                            </div>
 
-                    <div className="password">
-                       <label htmlFor="newPassword">Nova Senha</label>
-                        <input type="text" id="newPassword"/>
-                        <label htmlFor="confirmPassword">Confirmar Senha</label>
-                        <input type="text" id="confirmPassword"/>
-                    </div>
+                            <div className="password">
+                            <label htmlFor="newPassword">Nova Senha</label>
+                                <input type="text" id="newPassword"/>
+                                <label htmlFor="confirmPassword">Confirmar Senha</label>
+                                <input type="text" id="confirmPassword"/>
+                            </div>
 
-                    <button className="button" type="submit">Cadastrar</button>
-               </form>
+                            <button className="button" type="submit">Cadastrar</button>
+                    </form>
+                </section>    
             </div>
         </div>
     );
